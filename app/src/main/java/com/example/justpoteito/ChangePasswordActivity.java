14299e7f7b5a46c17e04f31298e873218e4bad9f
FabianaSilva15 <fabiana.silva@elorrieta-errekamari.com>
2023-01-30 18:59:22 +0100

    ChangePassword

--- app/src/main/java/com/example/justpoteito/ChangePasswordActivity.java
+++ app/src/main/java/com/example/justpoteito/ChangePasswordActivity.java
@@ -21,5 +21,40 @@ public class ChangePasswordActivity extends AppCompatActivity {
         confirmPassword = findViewById(R.id.editText_confirmPassword);
 
         changePass = findViewById(R.id.resetPassword_button);
+        (findViewById(R.id.resetPassword_button)).setOnClickListener(v -> {
+            if (changePasswordFormIsValid()) {
+                RequestResponse response = new NetworkUtilites(this).makeRequest(new ChangePasswordRequest(generateChangePasswordJson(), this));
+                Toast.makeText(this, response.getMessage(), Toast.LENGTH_SHORT).show();
+
+                if (response.isAccess()) {
+                    setFragmentLayout("sign_in");
+                }
+            }
+        });
     }
+
+    private boolean changePasswordFormIsValid(){
+
+    }
+
+    /*private boolean changePasswordFormIsValid() {
+        boolean isValid = true;
+
+        EditText password = ((EditText)findViewById(R.id.newPasswordTextViewReset));
+        EditText confirmPassword = ((EditText)findViewById(R.id.confirmNewPasswordTextViewReset));
+
+        if (!editTextIsValid(findViewById(R.id.usernameEditTextChangePass), 5, false)) isValid = false;
+        if (!editTextIsValid(findViewById(R.id.oldPasswordTextViewReset), 5, false)) isValid = false;
+        if (!editTextIsValid(password, 5, false)) isValid = false;
+        if (!editTextIsValid(confirmPassword, 5, false)) isValid = false;
+
+        if (!password.getText().toString().equals(confirmPassword.getText().toString())) {
+            password.setError(getString(R.string.passwords_do_not_match));
+            confirmPassword.setError(getString(R.string.passwords_do_not_match));
+            isValid = false;
+        }
+
+        return isValid;
+
+}*/
 }
